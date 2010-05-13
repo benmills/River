d0fea048e56705b33943f873603823d1628076f4
Ben Mills <benmills@bmdev-water.local>
2010-05-12 23:05:17 -0500

    sync

--- project/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from project.models import *
-
-class ProjectForm(forms.ModelForm):
-	class Meta:
-		model = Project
-		exclude = ('team')
--- project/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Project(models.Model):
-	title = models.CharField(max_length=250)
-	description = models.TextField()
-	team = models.ManyToManyField(User)
